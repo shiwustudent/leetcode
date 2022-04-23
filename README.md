a72cef6b9f3cd0a82f9e4b5b1ca8194c7d629105
shiwu <shiwustudent@gmail.com>
2022-04-23 12:23:19 +0800

    每日一题(4.23)

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 LeetCode
 ========
 
-| # | 名称  | 难度 |
-|---| -----  | ---------- |
-|396| 旋转数组（https://leetcode-cn.com/problems/rotate-function/） |中等|
+| #   | 名称                                                                                             | 难度    |
+|-----|------------------------------------------------------------------------------------------------|-------|
+| 3   | [无重复字符的最长子串](https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/) | 中等    |
+| 396 | [旋转数组](https://leetcode-cn.com/problems/rotate-function/)                                      | 中等    |
---
+++ src/com/darkgo/primary/LeetCode3.java
@@ -0,0 +1,38 @@
+package com.darkgo.primary;
+
+import java.util.HashSet;
+
+/**
+ * 无重复字符的最长子串
+ * 滑动窗口
+ * 通过HashSet保存不重复字符，如果重复，窗口向下滑动，并统计set中保存的字符数
+ * 时间复杂度O(n)
+ * 空间复杂度O(E) E代表出现的字符数
+* */
+public class LeetCode3 {
+    public static void main(String[] args) {
+//        String s = "abcabcbb";
+        String s = "au";
+        System.out.println(new Solution().lengthOfLongestSubstring(s));
+    }
+
+    static class Solution {
+        public int lengthOfLongestSubstring(String s) {
+            HashSet<Character> set = new HashSet<>();
+            int max = 0;
+            for (int i = 0; i < s.length(); i++) {
+                set.clear();
+                for (int j = i; j < s.length(); j++) {
+                    if (!set.contains(s.charAt(j))) {
+                        set.add(s.charAt(j));
+                    } else {
+                        max = Math.max(max, set.size());
+                        break;
+                    }
+                    max = Math.max(max, set.size());
+                }
+            }
+            return max;
+        }
+    }
+}
