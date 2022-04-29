a0bfaf5c8985ef4626a95a2965cf2872588ef8bb
shiwu <shiwustudent@gmail.com>
2022-04-29 15:42:15 +0800

    每日一题(4.29)

---
+++ src/com/darkgo/primary/LeetCode27.java
@@ -0,0 +1,30 @@
+package com.darkgo.primary;
+
+/**
+ * <p>
+ * 移除元素
+ * </p>
+ * 双指针问题
+ * @author ShiWu
+ * @since 2022/4/29
+ */
+public class LeetCode27 {
+    public static void main(String[] args) {
+        // int[] nums = {0, 1, 2, 2, 3, 0, 4, 2};
+        int[] nums = {3,2,2,3};
+        int val = 2;
+        System.out.println(new Solution().removeElement(nums, val));
+    }
+    static class Solution {
+        public int removeElement(int[] nums, int val) {
+            int slow = 0;
+            for (int i = 0; i < nums.length; i++) {
+                if (nums[i] != val) {
+                    nums[slow] = nums[i];
+                    slow++;
+                }
+            }
+            return slow;
+        }
+    }
+}
---
+++ src/com/darkgo/primary/LeetCode485.java
@@ -0,0 +1,33 @@
+package com.darkgo.primary;
+
+/**
+ * <p>
+ * 最大连续 1 的个数
+ * </p>
+ *
+ * @author ShiWu
+ * @since 2022/4/29
+ */
+public class LeetCode485 {
+
+    public static void main(String[] args) {
+        int[] nums = {1, 1, 0, 1, 1, 1};
+        System.out.println(new Solution().findMaxConsecutiveOnes(nums));
+    }
+    static class Solution {
+        public int findMaxConsecutiveOnes(int[] nums) {
+            int ans = 0;
+            int one = 0;
+            for (int num : nums) {
+                if (num == 0) {
+                    ans = Math.max(ans, one);
+                    one = 0;
+                } else {
+                    one++;
+                }
+            }
+            ans = Math.max(ans, one);
+            return ans;
+        }
+    }
+}
