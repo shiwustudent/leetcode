a8a18fd93ea2751b9a28a6955901e644e3dea705
shiwu <shiwustudent@gmail.com>
2022-06-18 18:23:40 +0800

    补充第二解

--- src/com/darkgo/primary/LeetCode566.java
+++ src/com/darkgo/primary/LeetCode566.java
@@ -30,10 +30,23 @@ public class LeetCode566 {
                 if (col == c - 1) {
                     row++;
                     col = 0;
-                }else {
+                } else {
                     col++;
                 }
             }
+        }
+        return ans;
+    }
+
+    private static int[][] matrixReshape1(int[][] mat, int r, int c) {
+        int m = mat.length;
+        int n = mat[0].length;
+        if (m * n != r * c) {
+            return mat;
+        }
+        int[][] ans = new int[m][n];
+        for (int i = 0; i < m * n; i++) {
+            ans[i/c][i%c] = mat[i/n][i%n];
         }
         return ans;
     }
