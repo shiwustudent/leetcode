ec4239e1387891e8c98fbbb7ed600b3db9468bf9
shiwu <shiwustudent@gmail.com>
2022-07-10 23:34:07 +0800

    补充第二种解法

--- src/com/darkgo/primary/LeetCode937.java
+++ src/com/darkgo/primary/LeetCode937.java
@@ -15,7 +15,7 @@ public class LeetCode937 {
     public static void main(String[] args) {
         // String[] logs = {"dig1 8 1 5 1", "let1 art can", "dig2 3 6", "let2 own kit dig", "let3 art zero"};
         // String[] logs = {"a1 9 2 3 1", "g1 act car", "zo4 4 7", "ab1 off key dog", "a8 act zoo"};
-        String[] logs = {"zoey i love you","lucas i love you","rong i love you"};
+        String[] logs = {"zoey i love you", "lucas i love you", "rong i love you"};
 
         System.out.println(Arrays.toString(reorderLogFiles(logs)));
     }
@@ -59,6 +59,52 @@ public class LeetCode937 {
             ans[i + list.size()] = digitList.get(i);
         }
         return ans;
+    }
 
+    private String[] reorderLogFiles1(String[] logs) {
+        int n = logs.length;
+        Pair[] arr = new Pair[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = new Pair(logs[i], i);
+        }
+        Arrays.sort(arr, new Comparator<Pair>() {
+            @Override
+            public int compare(Pair o1, Pair o2) {
+                String log1 = o1.log, log2 = o2.log;
+                int index1 = o1.index, index2 = o2.index;
+                String[] s1 = log1.split(" ");
+                String[] s2 = log2.split(" ");
+                boolean isDigit1 = Character.isDigit(s1[1].charAt(0));
+                boolean isDigit2 = Character.isDigit(s2[1].charAt(0));
+                if (isDigit1 && isDigit2) {
+                    return index1 - index2;
+                }
+                if (!isDigit1 && !isDigit2) {
+                    int sc = s1[1].compareTo(s2[1]);
+                    if (sc != 0) {
+                        return sc;
+                    }
+                    return s1[0].compareTo(s2[0]);
+                }
+                return isDigit1 ? 1 : 0;
+            }
+        });
+        String[] reordered = new String[n];
+        for (int i = 0; i < n; i++) {
+            reordered[i] = arr[i].log;
+        }
+        return reordered;
+
+    }
+
+
+    class Pair {
+        String log;
+        int index;
+
+        public Pair(String log, int index) {
+            this.log = log;
+            this.index = index;
+        }
     }
 }
