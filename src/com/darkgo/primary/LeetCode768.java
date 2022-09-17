e9351ed98c5812ac5933d82fed3f6814f3bcc760
shiwu <shiwustudent@gmail.com>
2022-09-17 18:24:49 +0800

    补充排序+哈希表的解法

--- src/com/darkgo/primary/LeetCode768.java
+++ src/com/darkgo/primary/LeetCode768.java
@@ -1,17 +1,18 @@
 package com.darkgo.primary;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
+import java.util.*;
 
 /**
  * <p>
  * 最多能完成排序的块 II
  * </p>
- *  //TODO 另一种解法
+ *
+ *
  * @author ShiWu
  * @since 2022/8/13
  */
 public class LeetCode768 {
+    // 单调栈
     public int maxChunksToSorted(int[] arr) {
         Deque<Integer> stack = new ArrayDeque<>();
         for (int i : arr) {
@@ -26,5 +27,30 @@ public class LeetCode768 {
             }
         }
         return stack.size();
+    }
+
+    //排序 map
+    public int maxChunksToSorted1(int[] arr) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int n = arr.length;
+        int[] sortedArr = new int[n];
+        System.arraycopy(arr, 0, sortedArr, 0, n);
+        Arrays.sort(sortedArr);
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            int x = arr[i], y = sortedArr[i];
+            map.put(x, map.getOrDefault(x, 0) + 1);
+            if (map.get(x) == 0) {
+                map.remove(x);
+            }
+            map.put(y, map.getOrDefault(y, 0) - 1);
+            if (map.get(y) == 0) {
+                map.remove(y);
+            }
+            if (map.isEmpty()) {
+                ans++;
+            }
+        }
+        return ans;
     }
 }
