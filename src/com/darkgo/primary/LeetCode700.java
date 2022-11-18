4f777493ad9a615385025db30fa7e8f1c03f3e47
shiwu <shiwustudent@gmail.com>
2022-11-18 10:23:06 +0800

    二叉搜索树中的搜索

---
+++ src/com/darkgo/primary/LeetCode700.java
@@ -0,0 +1,44 @@
+package com.darkgo.primary;
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author ShiWu
+ * @since 2022/11/17
+ */
+public class LeetCode700 {
+    private class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    private TreeNode searchBST(TreeNode root, int val) {
+        if (root == null) {
+            return null;
+        }
+        if (val == root.val) {
+            return root;
+        } else if (val < root.val) {
+            return searchBST(root.left, val);
+        } else if (val > root.val) {
+            return searchBST(root.right, val);
+        }
+        return null;
+    }
+}
